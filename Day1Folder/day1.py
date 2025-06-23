0b8042176cd840f2d076804e9c45427acf30e979
Computhink-CS138 <cs138@computhink.com.sg>
2025-06-23 04:30:38 +0000

    Jun 23, 2025, 4:30 AM

--- Day1Folder/day1.py
+++ Day1Folder/day1.py
@@ -1,40 +1,62 @@
-# Write all your codes for Day 1 here.
-# COMMENT out the previous task before going on to the next task
-print("hello from day1")
+# greetings()# Write all your codes for Day 1 here.
+# # COMMENT out the previous task before going on to the next task
+# print("hello from day1")
+# print("my name is brandon")
+# ########################################################################
+# # Task 1:
+# #print("my name is brandon")
+# # print("my school is pei chun public school")
+# ########################################################################
+# # Task 2:
+# print("if i have $2000 as a reward, i will buy a round trip to malaysia for myself ")
 
-########################################################################
-# Task 1:
+# ########################################################################
+# # Task 3:
+# # wallet = 100
+# # wallet = 69 + 69 * 69 - 69 / 69
+# # print(wallet)
+# animal1 = "tiger"
+# animal2 = "pufferfish"
+# animal3 = "deer"
+# print(animal1,animal2,animal3)
+# ########################################################################
+# # Task 4:
+# variable1 = 21
+# variable2 = 69
+# variable3 = 420
+# variable4 = 666
+# print (variable1 + variable2 - variable3 * variable4 / 2169420666)
 
 
+# ########################################################################
+# # Task 5:
+# name = "you"
+# food = "BBQ"
+# print (name + " said you" +  ' liked ' + food )
 
-########################################################################
-# Task 2:
-
-
-
-########################################################################
-# Task 3:
-
-
-
-########################################################################
-# Task 4:
-
-
-
-########################################################################
-# Task 5:
-
-
-
-########################################################################
-# Task 6:
+# ########################################################################
+# # Task 6:
+# def intro():
+#     print("nice to meet you")
+#     print("my name is brandon")
+# intro()
+# def greetings():
+#     print("my name is brandon")
+#     print("i am 11 years old")
+#     print("i am from pei chun public school")
+#     print("if i have $2000 as a reward, i will buy a round trip to malaysia for myself")
 
 
 
 ########################################################################
 # Task 7:
+def addition(num1, num2):
+    print( num1 + num2 )
 
+addition(69,69)
+addition(5,3)
+addition(243,236)
+addition(5000,3)
 
 
 ########################################################################
