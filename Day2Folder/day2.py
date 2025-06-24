6d6fb235b2b47680b06795c13b66f5bede3f550b
Computhink-CS138 <cs138@computhink.com.sg>
2025-06-24 02:00:09 +0000

    Jun 24, 2025, 2:00 AM

--- Day2Folder/day2.py
+++ Day2Folder/day2.py
@@ -2,7 +2,7 @@
 # COMMENT out the previous task before going on to the next task
 print("hello from day2")
 for count in range(69):
-    print("")
+    print("your mum is fat she ate ")
 ########################################################################
 # Task 1:
 
