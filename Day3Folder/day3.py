5633bed6359ba3250b32dacff3a18381706c61e5
Computhink-CS138 <cs138@computhink.com.sg>
2025-06-25 02:09:19 +0000

    Jun 25, 2025, 2:09 AM

--- Day3Folder/day3.py
+++ Day3Folder/day3.py
@@ -11,7 +11,7 @@
 # Task 1:
 yourname=input("what is your name")
 yourtitle=input("what is your title")
-yourcommand
+yourcommand=input("")
 
 
 ########################################################################
