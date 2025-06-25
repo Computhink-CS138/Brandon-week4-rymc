c1786f76c47d8545c37c80a8471756a3cbf99b9a
Computhink-CS138 <cs138@computhink.com.sg>
2025-06-25 01:49:04 +0000

    Jun 25, 2025, 1:49 AM

--- Day3Folder/day3.py
+++ Day3Folder/day3.py
@@ -3,7 +3,7 @@
 print("hello from day3")
 
 yourname = input("what is your name?")
-print(yourname)
+print(yourname + "")
 ########################################################################
 # Task 1:
 
