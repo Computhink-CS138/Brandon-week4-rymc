56554865c89a4bfa8802d6c94b204bac9cea4b5b
Computhink-CS138 <cs138@computhink.com.sg>
2025-06-25 02:44:50 +0000

    Jun 25, 2025, 2:44 AM

--- Day3Folder/day3.py
+++ Day3Folder/day3.py
@@ -34,7 +34,7 @@ unitcost = 54
 quantity = input("how many do u want to purchase?")
 quantity = int(quantity)
 total = unitcost * quantity
-print("")
+print("please pay me")
 ########################################################################
 # Task 5:
 
