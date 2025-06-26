a2fcfe683d703445c1e7d2142a68b1954085a73b
Computhink-CS138 <cs138@computhink.com.sg>
2025-06-26 01:58:46 +0000

    Jun 26, 2025, 1:58 AM

--- Day4Folder/day4.py
+++ Day4Folder/day4.py
@@ -13,7 +13,7 @@ for count in range(10):
 ########################################################################
 # Task 2:
 
-riddle = "what do you call a 3 deer in 3 herd with tree?"
+riddle = "what do you call a 3 deer in 3 herds with three ?"
 hidden = "thirceof a deer"
 
 
