b2313f3d589228d36e8c10ee325916335634bb97
Computhink-CS138 <cs138@computhink.com.sg>
2025-06-26 01:40:07 +0000

    Jun 26, 2025, 1:40 AM

--- Day4Folder/day4.py
+++ Day4Folder/day4.py
@@ -3,7 +3,7 @@
 print("hello from day4")
 import random
 for count in range(10):
-    dicevalue = random
+    dicevalue = random.randint
 ########################################################################
 # Task 1:
 
